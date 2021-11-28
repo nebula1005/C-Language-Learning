35c1a98de8572f5be29f285b998bb46d77751a2e
nebula1005 <1034691208@qq.com>
2021-11-29 00:25:33 +0800

    2020广工新生杯体验

---
+++ Competition/2020广工新生程序设计竞赛/J-简单的数学.c
@@ -0,0 +1,16 @@
+//Á´½Ó£ºhttps://ac.nowcoder.com/acm/problem/200313
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int t, i;
+    long long n, a;
+    scanf("%d", &t);
+    for (i = 0; i < t; i++)
+    {
+        scanf("%lld", &n);
+        a = pow(-1, n - 1) * n * (n + 1);
+        printf("%lld\n", a);
+    }
+    return 0;
+}

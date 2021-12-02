6364165abd75d19742fc28fba06efd2a3b2c73d9
nebula1005 <1034691208@qq.com>
2021-12-02 16:36:19 +0800

    实例9

---
+++ 100exercises/exercise_009.c
@@ -0,0 +1,17 @@
+﻿//要求输出国际象棋棋盘。（很无语）
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+            if ((i + j) % 2 == 0)
+                printf("%c%c", 219, 219);
+            else printf("  ");
+        printf("\n");
+    }
+    return 0;
+}

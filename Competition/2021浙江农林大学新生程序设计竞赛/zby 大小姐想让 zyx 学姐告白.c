d2bd78707989cd92572a724c2989f5b27d99ce65
nebula1005 <1034691208@qq.com>
2021-12-05 18:09:17 +0800

    2021浙农新生杯体验

---
+++ Competition/2021浙江农林大学新生程序设计竞赛/zby 大小姐想让 zyx 学姐告白.c
@@ -0,0 +1,140 @@
+﻿//链接：https://ac.nowcoder.com/acm/contest/25717/J
+//【学姐们的恋爱头脑战！】
+//zby大小姐（以下简称朱大小姐）想让zyx学姐（以下简称周少）告白，但是她不想成为先告白的一方,
+//于是她随机写了一些数字，想让周少找出这个数组中藏着多少 `520` 。
+//数字 `5`、`2` 、`0` 在该数组中出现的顺序可以是任意的，且出现的位置不必连续，以及每个数字只能被计数一次。
+//如果周少不能准确说出这个数组中 `520` 的个数，周少就要向朱大小姐告白。
+//周少受到辉夜大小姐和白银会长影响，深知在恋爱中先告白的一方就是输家，她当然不想成为输家，
+//于是她每次都能准确说对朱大小姐所给数组中 `520` 的个数，这样就可以避免成为先告白的一方了！
+//聪明的你，知道周少回答的数字是多少吗？
+//【今日的胜负：朱大小姐的败北。】
+#include <stdio.h>
+int main()
+{
+    int n, a, i, arr[1000000], j, k, count = 0, flag;
+    while (scanf("%d", &n) != EOF)
+    {
+        for (i = 0; i < (n - 1); i++)
+        {
+            scanf("%d ", &arr[i]);
+        }
+        scanf("%d", &arr[n - 1]);
+        for (i = 0; i < n; i++)
+        {
+            flag = 1;
+            if (arr[i] == 5)
+            {
+                for (j = (i + 1); j < n; j++)
+                {
+                    if (!flag)break;
+                    if (arr[j] == 2)
+                    {
+                        for (k = (j + 1); k < n; k++)
+                        {
+                            if (arr[k] == 0)
+                            {
+                                count++;
+                                arr[i] = 1;
+                                arr[j] = 1;
+                                arr[k] = 1;
+                                flag = 0;
+                                break;
+                            }
+                        }
+                    }
+                    else if (arr[j] == 0)
+                    {
+                        for (k = (j + 1); k < n; k++)
+                        {
+                            if (arr[k] == 2)
+                            {
+                                count++;
+                                arr[i] = 1;
+                                arr[j] = 1;
+                                arr[k] = 1;
+                                flag = 0;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            else if (arr[i] == 2)
+            {
+                for (j = (i + 1); j < n; j++)
+                {
+                    if (!flag)break;
+                    if (arr[j] == 5)
+                    {
+                        for (k = (j + 1); k < n; k++)
+                        {
+                            if (arr[k] == 0)
+                            {
+                                count++;
+                                arr[i] = 1;
+                                arr[j] = 1;
+                                arr[k] = 1;
+                                flag = 0;
+                                break;
+                            }
+                        }
+                    }
+                    else if (arr[j] == 0)
+                    {
+                        for (k = (j + 1); k < n; k++)
+                        {
+                            if (arr[k] == 5)
+                            {
+                                count++;
+                                arr[i] = 1;
+                                arr[j] = 1;
+                                arr[k] = 1;
+                                flag = 0;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            if (arr[i] == 0)
+            {
+                for (j = (i + 1); j < n; j++)
+                {
+                    if (!flag)break;
+                    if (arr[j] == 2)
+                    {
+                        for (k = (j + 1); k < n; k++)
+                        {
+                            if (arr[k] == 5)
+                            {
+                                count++;
+                                arr[i] = 1;
+                                arr[j] = 1;
+                                arr[k] = 1;
+                                flag = 0;
+                                break;
+                            }
+                        }
+                    }
+                    else if (arr[j] == 5)
+                    {
+                        for (k = (j + 1); k < n; k++)
+                        {
+                            if (arr[k] == 2)
+                            {
+                                count++;
+                                arr[i] = 1;
+                                arr[j] = 1;
+                                arr[k] = 1;
+                                flag = 0;
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    printf("%d", count);
+    return 0;
+}
---
+++ Competition/2021浙江农林大学新生程序设计竞赛/埃辛诺斯战刃.c
@@ -0,0 +1,35 @@
+﻿//链接：https://ac.nowcoder.com/acm/contest/25717/D
+//打印一把超级炫酷的埃辛诺斯战刃！
+//埃辛诺斯战刃由两个等腰直角三角形和中间的连接柄组成，其中直角边称之为刀身长，连接柄称之为柄长。
+//你是一名专门为恶魔猎手打造武器的工匠，请你设计一个程序，打印一把刀身长为 n, 柄长为 m 的埃辛诺斯战刃。
+#include <stdio.h>
+int main()
+{
+    int n, m, i, j;
+    scanf("%d %d", &n, &m);
+    for (i = 1; i <= n; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < (n - 1); j++)
+        {
+            printf(" ");
+        }
+        printf("*\n");
+    }
+    for (i = n; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

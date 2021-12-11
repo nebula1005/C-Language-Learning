1c583e41f5d6790f965819ed90c693ef8a7858d7
nebula1005 <1034691208@qq.com>
2021-12-11 23:51:20 +0800

    实例15

---
+++ 100exercises/exercise_015.c
@@ -0,0 +1,13 @@
+﻿//利用条件运算符（三目运算符）的嵌套来完成此题：学习成绩>=90分的同学用A表示，60-89分之间的用B表示，60分以下的用C表示。
+
+#include <stdio.h>
+
+int main()
+{
+    int score;
+    char garde;
+    printf("请输入分数：");
+    scanf("%d", &score);
+    printf("%c", score >= 90 ? 'A' : (score < 60 ? 'C' : 'B'));
+    return 0;
+}

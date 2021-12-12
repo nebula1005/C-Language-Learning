12a7e61367265ebb14d1a01c72c022fbd5aa602a
nebula1005 <1034691208@qq.com>
2021-12-12 23:47:21 +0800

    实例16

---
+++ 100exercises/exercise_016.c
@@ -0,0 +1,27 @@
+﻿//输入两个正整数m和n，求其最大公约数和最小公倍数
+
+#include <stdio.h>
+
+int main()
+{
+    int m, n, r, o;
+    printf("请输入两个正整数：");
+    scanf("%d,%d", &m, &n);
+    o = m * n;
+    //最大公约数判断
+    if (m < n)    //判断前提是m>=n
+    {
+        r = m;
+        m = n;
+        n = r;
+    }
+    r = m % n;
+    while (r > 0)
+    {
+        m = n;
+        n = r;
+        r = m % n;
+    }
+    printf("这两个数的最大公约数是%d，最小公倍数是%d。", n, o / n);
+    return 0;
+}

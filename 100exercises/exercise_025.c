d107265902542b68755acff8fd5b9851f2149ffe
nebula1005 <1034691208@qq.com>
2021-12-23 00:18:53 +0800

    实例25

---
+++ 100exercises/exercise_025.c
@@ -0,0 +1,26 @@
+﻿//求1+2!+3!+...+20!的和。
+
+#include <stdio.h>
+
+//定义阶乘函数
+long long fac(int x)
+{
+    int i;
+    long long y = x;
+    for (i = 1; i < x; i++)
+    {
+        y *= (x - i);
+    }
+    return y;
+}
+
+int main()
+{
+    int i;
+    long long sum = 0;
+    for (i = 1; i <= 20; i++)
+    {
+        sum += fac(i);
+    }
+    printf("%lld", sum);
+}

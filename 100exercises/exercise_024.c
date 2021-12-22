bd6b57142eb84f849986089b0e911441dedcfbc9
nebula1005 <1034691208@qq.com>
2021-12-22 22:00:27 +0800

    实例24

---
+++ 100exercises/exercise_024.c
@@ -0,0 +1,17 @@
+﻿//有一分数序列：2/1，3/2，5/3，8/5，13/8，21/13...求出这个数列的前20项之和。
+
+#include <stdio.h>
+
+int main()
+{
+    int a = 2, i, r;
+    float b = 1.0, sum = 0;
+    for (i = 0; i < 20; i++)
+    {
+        sum += a / b;
+        r = a;  //中转
+        a = a + b;
+        b = r;
+    }
+    printf("%f", sum);
+}

4f9f1fb35849188e7cf39154c0c690b3d12b51fb
nebula1005 <1034691208@qq.com>
2022-01-20 21:07:43 +0800

    实例38

---
+++ 100exercises/exercise_038.c
@@ -0,0 +1,22 @@
+﻿//求一个3*3矩阵对角线元素之和
+
+#include <stdio.h>
+#define N 3  //便于修改矩阵
+int main()
+{
+    int i, j, arr[N][N], sum = 0;
+    printf("请输入一个3*3矩阵：\n");
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for (i = 0; i < N; i++)
+    {
+        sum += arr[i][i];
+    }
+    printf("对角线之和为%d", sum);
+    return 0;
+}

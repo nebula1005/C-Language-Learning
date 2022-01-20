ea0b58cdfa581318f163f7660ff5f73138635474
nebula1005 <1034691208@qq.com>
2022-01-20 21:05:48 +0800

    实例34

---
+++ 100exercises/exercise_034.c
@@ -0,0 +1,18 @@
+﻿//函数的调用
+
+#include <stdio.h>
+void hello_world()
+{
+    printf("Hello, world!\n");
+}
+void three_hellos()
+{
+    int counter;
+    for (counter = 1; counter <= 3; counter++)
+        hello_world();/*调用此函数*/
+}
+int main()
+{
+    three_hellos();/*调用此函数*/
+    return 0;
+}
